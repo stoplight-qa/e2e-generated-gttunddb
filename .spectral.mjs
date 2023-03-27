cff8d51abc877610c5c48065faebfc517ad3d2da
qa-scrqi-4-ancl@example.com <qa-scrqi-4-ancl@example.com>
2023-03-27 18:49:53 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-15814.stoplight-dev.com/api/v1/projects/cHJqOjYzNw/spectral.js?branch_slug=main&token=dec0d505-5cac-4441-a72e-36b6437ba943";
+export default { extends: ruleset };
